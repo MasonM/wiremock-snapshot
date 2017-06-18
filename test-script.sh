1a24a74b9a4700cba063baa7276491d1f0bfe698
Mason Malone <mason@masonm.org>
2017-06-17 17:50:40 -0700

    Improvements to test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -1,24 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-# Basic test script
+# Basic test script. Supply JSON string with parameters.
 
 set -e
 
-#echo "Launching Wiremock and setting up proxying"
-#python -m SimpleHTTPServer &
-#java -jar build/libs/wiremock-snapshot-standalone-*.jar &
+echo "Launching Wiremock and setting up proxying"
+
+python -m SimpleHTTPServer 1>/dev/null 2>/dev/null &
+PYTHON_PID=$!
+java -jar build/libs/wiremock-snapshot-standalone-*.jar 1>/dev/null 2>/dev/null & 
+WIREMOCK_PID=$!
+
+echo -n "Waiting for Wiremock to start up."
+
+until $(curl --output /dev/null --silent --head http://localhost:8080); do
+   echo -n '.'
+   sleep 5
+done
+
+echo -e "done\nCreating proxy mapping"
 
 curl -s -d '{
-    "request": { "urlPattern": ".*" },
-    "response": {
-	"proxyBaseUrl": "http://localhost:8000"
+   "request": { "urlPattern": ".*" },
+   "response": {
+      "proxyBaseUrl": "http://localhost:8000"
     }
-}' http://localhost:8080/__admin/mappings
+}' http://localhost:8080/__admin/mappings > /dev/null
 
-echo "Making request to LICENSE and README.md"
+echo "Making requests"
+curl -X POST -s http://localhost:8080/src/main/java/com/github/masonm/wiremock/SnapshotExtension.java > /dev/null
+curl -s http://localhost:8080/README.md > /dev/null
 curl -s http://localhost:8080/LICENSE > /dev/null
 curl -s http://localhost:8080/README.md > /dev/null
+curl -s http://localhost:8080/README.md > /dev/null
 
-echo "Calling snapshot API. Should return two stub mappings:"
-#curl -s -X POST http://localhost:8080/__admin/recordings/snapshot | jq
-curl -s -d '{ "outputFormat": "full" }' http://localhost:8080/__admin/recordings/snapshot | jq
+JSON=$1
+if [[ -z $JSON ]]; then
+   JSON='{ "outputFormat": "full", "persist": false, "repeatsAsScenarios": true }'
+fi
+echo "Calling snapshot API with '${JSON}'"
+curl -s -X POST -d "${JSON}" http://localhost:8080/__admin/recordings/snapshot | jq
+
+kill $PYTHON_PID $WIREMOCK_PID
